a488067e88bb47d11c8073c32ae98a143d5b2157
sozohoy <sozohoy@gmail.com>
2024-05-12 23:17:01 +0900

    chore: 폴더링 위한 더미파일 추가(#1)

---
+++ GsHometown-iOS/GsHometown-iOS/View/Detail/dummy4.swift
@@ -0,0 +1,9 @@
+//
+//  dummy.swift
+//  GsHometown-iOS
+//
+//  Created by 한지석 on 5/12/24.
+//
+
+import Foundation
+
---
+++ GsHometown-iOS/GsHometown-iOS/View/Home/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GsHometown-iOS
+//
+//  Created by 한지석 on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
