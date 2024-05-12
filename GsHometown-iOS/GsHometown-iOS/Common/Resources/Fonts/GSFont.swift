ee6e5e6becab707f22c534d8cf42aafb5ab551a8
sozohoy <sozohoy@gmail.com>
2024-05-12 23:16:09 +0900

    feat: 폰트 정의(#1)

---
+++ GsHometown-iOS/GsHometown-iOS/Common/Resources/Fonts/GSFont.swift
@@ -0,0 +1,31 @@
+//
+//  GSFont.swift
+//  GsHometown-iOS
+//
+//  Created by 한지석 on 5/12/24.
+//
+
+import Foundation
+import UIKit
+
+enum GSFont {
+    static let h1 = UIFont(name: "Pretendard-Bold", size: 24)
+    static let h2 = UIFont(name: "Pretendard-Bold", size: 21)
+    static let h3 = UIFont(name: "Pretendard-SemiBold", size: 20)
+    static let h4 = UIFont(name: "Pretendard-Bold", size: 18)
+
+    static let b1 = UIFont(name: "Pretendard-SemiBold", size: 16)
+    static let b2s = UIFont(name: "Pretendard-SemiBold", size: 14)
+    static let b2m = UIFont(name: "Pretendard-Medium", size: 14)
+    static let b3b = UIFont(name: "Pretendard-Bold", size: 13)
+    static let b3s = UIFont(name: "Pretendard-SemiBold", size: 13)
+    static let b3m = UIFont(name: "Pretendard-Medium", size: 13)
+    static let b4s = UIFont(name: "Pretendard-SemiBold", size: 12)
+    static let b4m = UIFont(name: "Pretendard-Medium", size: 12)
+    static let b4r = UIFont(name: "Pretendard-Regular", size: 12)
+    static let b5s = UIFont(name: "Pretendard-SemiBold", size: 11)
+    static let b5m = UIFont(name: "Pretendard-Medium", size: 11)
+
+    static let d1s = UIFont(name: "Pretendard-SemiBold", size: 10)
+    static let d1m = UIFont(name: "Pretendard-Medium", size: 10)
+}
