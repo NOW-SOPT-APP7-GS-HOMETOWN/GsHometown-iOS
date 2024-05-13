96cda925aed09b62f5ddd78caa4000381491bf44
sozohoy <sozohoy@gmail.com>
2024-05-13 20:01:58 +0900

    feat: Cell Identifier을 타입 이름으로 지정하는 기능(#4)

--- GsHometown-iOS/GsHometown-iOS/Common/Extension/UICollectionViewCell+.swift
+++ GsHometown-iOS/GsHometown-iOS/Common/Extension/UICollectionViewCell+.swift
@@ -1,8 +1,14 @@
 //
-//  UICollectionViewCell.swift
+//  UICollectionViewCell+.swift
 //  GsHometown-iOS
 //
 //  Created by 한지석 on 5/13/24.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionViewCell {
+    static var cellIdentifier: String {
+        return String(describing: self)
+    }
+}
