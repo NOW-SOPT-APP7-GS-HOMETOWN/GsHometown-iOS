948c283ffdd5510ed2601435f493bb3b5c820ed5
sozohoy <sozohoy@gmail.com>
2024-05-13 19:10:31 +0900

    chore: Add files(#4)

---
+++ GsHometown-iOS/GsHometown-iOS/View/Home/ConvenienceCell.swift
@@ -0,0 +1,8 @@
+//
+//  ConvenienceCell.swift
+//  GsHometown-iOS
+//
+//  Created by 한지석 on 5/13/24.
+//
+
+import Foundation
---
+++ GsHometown-iOS/GsHometown-iOS/View/Home/PageCell.swift
@@ -0,0 +1,8 @@
+//
+//  PageCell.swift
+//  GsHometown-iOS
+//
+//  Created by 한지석 on 5/13/24.
+//
+
+import Foundation
