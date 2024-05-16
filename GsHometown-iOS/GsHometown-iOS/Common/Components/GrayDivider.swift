ad97051315ce6114ec9be22719bd705a88fc8ad9
cirtuare <cirtuare@gmail.com>
2024-05-17 03:00:39 +0900

    feat: GrayDivider UI

---
+++ GsHometown-iOS/GsHometown-iOS/Common/Components/GrayDivider.swift
@@ -0,0 +1,17 @@
+//
+//  GrayDivider.swift
+//  GsHometown-iOS
+//
+//  Created by 이수민 on 5/17/24.
+//
+
+import UIKit
+
+import UIKit
+
+func grayDivider() -> UIView {
+    let view = UIView()
+    view.backgroundColor = GSColor.grey03
+    return view
+}
+
