167ff529ea231b650fe5bb7cf04efc8451631105
cirtuare <cirtuare@gmail.com>
2024-05-17 02:55:38 +0900

    feat: SegmentControl UI

---
+++ GsHometown-iOS/GsHometown-iOS/Common/Components/SegmentControl.swift
@@ -0,0 +1,43 @@
+//
+//  SegmentControl.swift
+//  GsHometown-iOS
+//
+//  Created by 이수민 on 5/17/24.
+//
+
+import UIKit
+
+class SegmentControl: UISegmentedControl {
+    
+    private var segmentInset: CGFloat = 8
+    
+    override init(items: [Any]?) {
+        super.init(items: items)
+        selectedSegmentIndex = 0
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews(){
+        super.layoutSubviews()
+        self.backgroundColor = GSColor.grey05
+        self.layer.cornerRadius =  24
+        self.layer.masksToBounds = true
+
+        let selectedImageViewIndex = numberOfSegments
+        if let selectedImageView = subviews[selectedImageViewIndex] as? UIImageView
+        {
+            selectedImageView.backgroundColor = GSColor.grey00
+            selectedImageView.image = nil
+            
+            selectedImageView.bounds = selectedImageView.bounds.insetBy(dx: segmentInset, dy: segmentInset)
+            
+            selectedImageView.layer.masksToBounds = true
+            selectedImageView.layer.cornerRadius = 20
+            
+            selectedImageView.layer.removeAnimation(forKey: "SelectionBounds")
+        }
+    }
+}
