6ae6496e8e3e2fed538b0d79847379ce5ece1c13
luckyyy <oyslucy@naver.com>
2024-05-17 17:14:30 +0900

    add: DetailViewController

---
+++ GsHometown-iOS/GsHometown-iOS/View/Detail/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  GsHometown-iOS
+//
+//  Created by 오연서 on 5/17/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
