51c27e22f9dc4386cccadbcad3f7689974ca8de5
sozohoy <sozohoy@gmail.com>
2024-05-21 01:58:38 +0900

    add: DTO(#23)

---
+++ GsHometown-iOS/GsHometown-iOS/Network/DTO/Request/DTO+PostLikeRequest.swift
@@ -0,0 +1,8 @@
+//
+//  DTO+PostLikeRequest.swift
+//  GsHometown-iOS
+//
+//  Created by 한지석 on 5/21/24.
+//
+
+import Foundation
---
+++ GsHometown-iOS/GsHometown-iOS/Network/DTO/Response/DTO+GetDetailProductResponse.swift
@@ -0,0 +1,8 @@
+//
+//  DTO+GetDetailProductResponse.swift
+//  GsHometown-iOS
+//
+//  Created by 한지석 on 5/21/24.
+//
+
+import Foundation
