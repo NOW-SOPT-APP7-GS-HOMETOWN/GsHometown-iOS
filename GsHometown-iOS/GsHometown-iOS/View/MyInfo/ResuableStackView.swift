4800bb92cbf080cbef6bde0f5ebe98da64e315b7
cirtuare <cirtuare@gmail.com>
2024-05-22 02:33:37 +0900

    feat: ReusableStackView 구현(#25)

---
+++ GsHometown-iOS/GsHometown-iOS/View/MyInfo/ResuableStackView.swift
@@ -0,0 +1,51 @@
+//
+//  ResuableTwoStackView.swift
+//  GsHometown-iOS
+//
+//  Created by 이수민 on 5/22/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ResuableStackView: UIStackView {
+    
+    init(firstMenu: String, secondMenu: String?) {
+        super.init(frame: .zero)
+        setStyle()
+        setUI(firstMenu: firstMenu, secondMenu: secondMenu)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI(firstMenu : String, secondMenu : String?){
+        let firstButton = makeMenuButton(title: firstMenu)
+        self.addArrangedSubview(firstButton)
+        if let secondMenu = secondMenu {
+            let secondButton = makeMenuButton(title: secondMenu)
+            self.addArrangedSubview(secondButton)
+        }
+    }
+    
+    private func setStyle(){
+        self.isLayoutMarginsRelativeArrangement = true
+        self.axis = .horizontal
+        self.layoutMargins.top = 12
+        self.layoutMargins.bottom = 12
+        self.distribution = .fillEqually
+    }
+    
+    private func makeMenuButton(title : String) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(GSColor.grey10, for: .normal)
+        if let titleLabel = button.titleLabel{
+            titleLabel.font = GSFont.b2m
+        }
+        return button
+    }
+}
---
+++ GsHometown-iOS/GsHometown-iOS/View/MyInfo/View/UserMenuView.swift
@@ -0,0 +1,22 @@
+//
+//  UserMenuView.swift
+//  GsHometown-iOS
+//
+//  Created by 이수민 on 5/22/24.
+//
+
+import UIKit
+
+class UserMenuView: UIView {
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+
+}
