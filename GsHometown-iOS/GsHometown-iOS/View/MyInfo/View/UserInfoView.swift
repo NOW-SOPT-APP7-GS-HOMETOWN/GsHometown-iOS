9fcd410f182860d7cc44696aa49e664c4417e4b6
cirtuare <cirtuare@gmail.com>
2024-05-21 12:21:27 +0900

    add: MyInfoVC

---
+++ GsHometown-iOS/GsHometown-iOS/View/MyInfo/View/UserInfoView.swift
@@ -0,0 +1,29 @@
+//
+//  UserInfoView.swift
+//  GsHometown-iOS
+//
+//  Created by 이수민 on 5/21/24.
+//
+
+import UIKit
+
+class UserInfoView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
