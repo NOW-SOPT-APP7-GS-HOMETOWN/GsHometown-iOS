00d36e5db32bc604801cc9760f13e0cde692d776
sozohoy <sozohoy@gmail.com>
2024-05-22 20:43:56 +0900

    feat: MenuCollectionView 내부로 로직 이동 및 제약 설정(#31)

--- GsHometown-iOS/GsHometown-iOS/View/PreOrder/View/MainView/MenuCollectionView.swift
+++ GsHometown-iOS/GsHometown-iOS/View/PreOrder/View/MainView/MenuCollectionView.swift
@@ -7,20 +7,54 @@
 
 import UIKit
 
+import SnapKit
+
 class MenuCollectionView: UIView, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    lazy var menuCollectionView : UICollectionView = {
+
+    var menuCollectionView : UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isScrollEnabled = false
+        collectionView.backgroundColor = GSColor.grey00
         return collectionView
     }()
-    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(menuCollectionView)
+        setAutoLayout()
+        registerCell()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setAutoLayout() {
+        menuCollectionView.snp.makeConstraints {
+            $0.verticalEdges.horizontalEdges.equalToSuperview()
+        }
+    }
+
+    func registerCell() {
+        menuCollectionView.register(
+            MenuCollectionViewCell.self,
+            forCellWithReuseIdentifier: MenuCollectionViewCell.identifier
+        )
+        menuCollectionView.delegate = self
+        menuCollectionView.dataSource = self
+        menuCollectionView.reloadData()
+    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return MenuType.allCases.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MenuCollectionViewCell.identifier, for: indexPath) as? MenuCollectionViewCell else { return UICollectionViewCell() }
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: MenuCollectionViewCell.identifier,
+            for: indexPath
+        ) as? MenuCollectionViewCell else { return UICollectionViewCell() }
         cell.bindData(type: MenuType(rawValue: indexPath.row)!)
         return cell
     }
@@ -30,11 +64,11 @@ class MenuCollectionView: UIView, UICollectionViewDataSource, UICollectionViewDe
         let width = screenWidth / (4+24*3/67)
         return CGSize(width: width, height: 86)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         let screenWidth = UIScreen.main.bounds.width - 35
         let width = screenWidth / (4+24*3/67)
