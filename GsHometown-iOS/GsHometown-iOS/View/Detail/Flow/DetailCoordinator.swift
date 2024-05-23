9a7a4888e9c933e19ece88c4a373e858ab347d1a
sozohoy <sozohoy@gmail.com>
2024-05-23 20:19:59 +0900

    feat: 코디네이터 구현(#34)

--- GsHometown-iOS/GsHometown-iOS/View/Detail/Flow/DetailCoordinator.swift
+++ GsHometown-iOS/GsHometown-iOS/View/Detail/Flow/DetailCoordinator.swift
@@ -5,4 +5,18 @@
 //  Created by 한지석 on 5/22/24.
 //
 
-import Foundation
+import UIKit
+
+final class DetailCoordinator: FlowCoordinator {
+    var presentationStyle: PresentationStyle
+    weak var navigationController: UINavigationController?
+
+    init(presentationStyle: PresentationStyle) {
+        self.presentationStyle = presentationStyle
+    }
+
+    func initScene() -> UIViewController {
+        let detailViewController = DetailViewController()
+        return detailViewController
+    }
+}
--- GsHometown-iOS/GsHometown-iOS/View/PreOrder/Flow/PreOrderCoordinator.swift
+++ GsHometown-iOS/GsHometown-iOS/View/PreOrder/Flow/PreOrderCoordinator.swift
@@ -5,4 +5,29 @@
 //  Created by 한지석 on 5/22/24.
 //
 
-import Foundation
+import UIKit
+
+final class PreOrderCoordinator: FlowCoordinator {
+    var presentationStyle: PresentationStyle
+    weak var navigationController: UINavigationController?
+
+    init(presentationStyle: PresentationStyle) {
+        self.presentationStyle = presentationStyle
+    }
+
+    func initScene() -> UIViewController {
+        let preOrderViewController = PreOrderViewController(delegate: self)
+        return preOrderViewController
+    }
+}
+
+extension PreOrderCoordinator: PreOrderCoordinatorDelegate {
+    func goToDetailView() {
+        guard let navigationController = navigationController else { return }
+        DetailCoordinator(
+            presentationStyle: .push(
+                navigationController: navigationController
+            )
+        ).start()
+    }
+}
