bf54a6f54b2f686b697ea8ddc467151155d4b4b9
sozohoy <sozohoy@gmail.com>
2024-05-23 20:19:34 +0900

    feat: AppCoordinator 구현, 최상위 뷰 선언(#34)

--- GsHometown-iOS/GsHometown-iOS/Application/AppCoordinator.swift
+++ GsHometown-iOS/GsHometown-iOS/Application/AppCoordinator.swift
@@ -5,4 +5,27 @@
 //  Created by 한지석 on 5/23/24.
 //
 
-import Foundation
+import UIKit
+
+final class AppCoordinator: Coordinator {
+
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.navigationController.navigationBar.isHidden = true
+    }
+
+    func start() {
+        self.goToTabBar()
+    }
+
+    func goToTabBar() {
+        GSTabBarCoordinator(
+            presentationStyle: .push(
+                navigationController: navigationController
+            )
+        ).start()
+    }
+
+}
