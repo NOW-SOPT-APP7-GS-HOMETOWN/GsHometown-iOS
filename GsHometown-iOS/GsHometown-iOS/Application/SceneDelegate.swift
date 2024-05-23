a06dae8d5f1c175c792978d38f48d039d0d92ebb
sozohoy <sozohoy@gmail.com>
2024-05-23 20:20:38 +0900

    feat: SceneDelegate 구현(#34)

--- GsHometown-iOS/GsHometown-iOS/Application/SceneDelegate.swift
+++ GsHometown-iOS/GsHometown-iOS/Application/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = scene as? UIWindowScene else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: DetailViewController())
+        let navigationController = UINavigationController()
+        AppCoordinator(navigationController: navigationController).start()
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
     }
